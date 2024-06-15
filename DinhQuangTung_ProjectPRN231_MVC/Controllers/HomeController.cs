f6069aa526a367156093a7af26d51983e0f00c7c
tung2 <tung2@DESKTOP-B4C2LRF>
2024-06-15 20:35:22 +0700

    Add project files.

---
+++ DinhQuangTung_ProjectPRN231_MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using DinhQuangTung_ProjectPRN231_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DinhQuangTung_ProjectPRN231_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DinhQuangTung_ProjectPRN231_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DinhQuangTung_ProjectPRN231_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
