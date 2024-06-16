4e55a09166db5e4a417201e8f256a47a37baeac1
tung2 <tung2@DESKTOP-B4C2LRF>
2024-06-16 10:43:47 +0700

    Create entities

---
+++ Data/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int SortOrder {  get; set; }
+        public bool IsShowOnHome { get; set; }
+        public int? ParenID { get; set; }
+        public Status Status { get; set; }  
+    }
+}
--- README.txt
+++ README.txt
@@ -1,1 +1,8 @@
 # ASP.NET CORE PROJECT FROM DINH QUANG TUNG 
+## Technologies 
+-ASP.Net
+-Entity Framework 
+## Install Packages 
+-Microsoft.EntityFrameworkCore.Design
+-Microsoft.EntityFrameworkCore.SqlServer
+-Microsoft.EntityFrameworkCore.Tools
